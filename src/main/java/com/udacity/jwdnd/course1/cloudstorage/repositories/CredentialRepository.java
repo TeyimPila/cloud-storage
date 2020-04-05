5949f549ebbb9058563794c66e3177f4a8ec3271
Pila <pat@medicuja.com>
2020-04-05 17:11:12 +0200

    Complete implementation

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/repositories/CredentialRepository.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/repositories/CredentialRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Mapper
 public interface CredentialRepository {
 
-    @Insert("INSERT INTO CREDENTIALS(URL, USERNAME, KEY, PASSWORD, USERID) VALUES (#{url}, #{username}, #{key}, #{password}, #{user.userId})")
+    @Insert("INSERT INTO CREDENTIALS(URL, USERNAME, `KEY`, PASSWORD, USERID) VALUES (#{url}, #{username}, #{key}, #{password}, #{user.userId})")
     Integer save(Credential file);
 
     @Select("SELECT * FROM CREDENTIALS WHERE CREDENTIALID = #{id}")
@@ -23,7 +23,7 @@ public interface CredentialRepository {
     @Select("SELECT * FROM CREDENTIALS WHERE USERID = #{userId}")
     List<Credential> findByUser(User user);
 
-    @Insert("UPDATE CREDENTIALS SET USERNAME=#{username}, KEY=#{key}, PASSWORD=#{password} WHERE CREDENTIALID=#{credentialId}")
+    @Insert("UPDATE CREDENTIALS SET USERNAME=#{username}, `KEY`=#{key}, PASSWORD=#{password} WHERE CREDENTIALID=#{credentialId}")
     Integer update(Credential credential);
 
     @Delete("DELETE FROM CREDENTIALS WHERE CREDENTIALID = #{credId}")
