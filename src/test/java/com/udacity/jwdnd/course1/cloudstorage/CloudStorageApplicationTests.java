10a3adfe32ab0ed4e47db22d07fb7816ec57501f
Pila <pat@medicuja.com>
2020-04-05 18:04:52 +0200

    Test authentication

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -2,7 +2,9 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -10,32 +12,92 @@ import org.springframework.boot.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	private WebDriver driver;
+    private WebDriver driver;
 
-	@BeforeAll
-	static void beforeAll() {
-		WebDriverManager.chromedriver().setup();
-	}
+    @BeforeAll
+    static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-	@BeforeEach
-	public void beforeEach() {
-		this.driver = new ChromeDriver();
-	}
+    @BeforeEach
+    public void beforeEach() {
+        this.driver = new ChromeDriver();
+    }
 
-	@AfterEach
-	public void afterEach() {
-		if (this.driver != null) {
-			driver.quit();
-		}
-	}
+    @AfterEach
+    public void afterEach() {
+        if (this.driver != null) {
+            driver.quit();
+        }
+    }
 
-	@Test
-	public void getLoginPage() {
-		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
-	}
+    @Test
+    public void getLoginPage() {
+        driver.get("http://localhost:" + this.port + "/login");
+        Assertions.assertEquals("Login", driver.getTitle());
+    }
+
+    @Test
+    public void validRegistrationTest() {
+
+        String username = "user@gmail.com";
+        String passwordVal = "Test@12345";
+        String homeUrl = "http://localhost:" + this.port + "/home";
+        String signupUrl = "http://localhost:" + this.port + "/signup";
+        String loginurl = "http://localhost:" + this.port + "/login";
+
+        driver.get(signupUrl);
+        WebElement companyName = driver.findElement(By.name("firstName"));
+        companyName.sendKeys("FName");
+
+        WebElement fullName = driver.findElement(By.name("lastName"));
+        fullName.sendKeys("LName");
+
+        WebElement email = driver.findElement(By.name("username"));
+        email.sendKeys(username);
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys(passwordVal);
+
+        WebElement signUp = driver.findElement(By.xpath("//button[text()='Register User']"));
+        signUp.click();
+
+        driver.get(homeUrl);
+
+        String destination = driver.getCurrentUrl();
+
+        Assertions.assertNotEquals(homeUrl, destination);
+
+
+        driver.navigate().to(loginurl);
+
+        email = driver.findElement(By.name("username"));
+        email.sendKeys(username);
+
+        password = driver.findElement(By.name("password"));
+        password.sendKeys(passwordVal);
+
+        WebElement login = driver.findElement(By.xpath("//button[text()='Login']"));
+        login.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertEquals(currentUrl, homeUrl);
+
+        // After login, user can access home
+		driver.get(homeUrl);
+		destination = driver.getCurrentUrl();
+		Assertions.assertEquals(homeUrl, destination);
+
+		WebElement logout = driver.findElement(By.xpath("//button[text()='Logout']"));
+		logout.click();
+
+		driver.navigate().to(homeUrl);
+		destination = driver.getCurrentUrl();
+		Assertions.assertNotEquals(homeUrl, destination);
+
+    }
 
 }
